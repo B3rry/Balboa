907d57fa1cbd3865b06e266e38327efe03c8a317
Jon Berry <jon@jonberry.co>
2020-05-10 10:48:15 -0400

    Updated message reciept and dispatch comments
    Signed-off-by: Jon Berry <jon@jonberry.co>

--- messages/message_dispatcher.py
+++ messages/message_dispatcher.py
@@ -3,6 +3,25 @@ import os
 import json
 import sys
 
+# ============= Message Dispatcher .py ============= #
+# Currently, Message Dispatcher handles the sending of responses to messages sent
+# to the bot's Reddit account using PRAW.
+#
+# The long term vision for this code is to handle the sending of all outgoing
+# messages. This solution will need to:
+# [ ] Standardize the data model for the definition of distribution with enough flexibility to support abstract response types 
+#     [ ]: Reddit compose new message
+#     [ ]: Reddit reply to existing message
+#     [ ]: Reddit send modmail
+#     [ ]: Slack reply in channel
+#     [ ]: Slack reply in dm
+#     [ ]: Slack set channel status
+# [ ] Manage the distribution of messages to varous response processes.
+#
+# [TODO]: Standardize the handling of various messaging integrations within this dir. Treat message-dispatcher as the way to route to a response type.
+# [TODO]: Templating!
+# [TODO]: Dynamic composure of messages, (headers, footers for different types, etc)
+
 class MessageDispatcher:
     def __init__(self, payload, response, reddit):
         self.payload = payload
--- messages/message_receiver.py
+++ messages/message_receiver.py
@@ -4,6 +4,18 @@ import json
 import sys
 from actions.action_request import ActionRequest
 
+# ============= Message Receiver .py ============= #
+# Currently, Message Receiver handles the retrieval of messages from the inbox
+# of the bot's Reddit account using PRAW.
+#
+# The long term vision for this code is to handle the management of all incoming
+# data. This solution will need to:
+# [ ] Handle incoming messages sent via the Slack API by way of the flask
+# [ ] Handle frequent, repeated requests to the Reddit API to check for messages
+# [ ] Exist, or augment the current `bot.py` loop model. *Big implications*
+#
+# [TODO]: Standardize the handling of various messaging integrations within this dir. Treat message-receiver as the way to initiate a dispatch.
+
 class MessageReceiver:
     def __init__(self, reddit):
         # passing a param and conditionally loads the data is maybe how we'd want do this. (+ a dict of service info?)
